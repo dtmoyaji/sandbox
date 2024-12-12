1ce1ea8b6f44717952604c66de6d389052ab56cd
dtmoyaji <156999048+tax-mux@users.noreply.github.com>
2024-12-12 15:40:03 +0900

    first commit

---
+++ nodejs/main.mjs
@@ -0,0 +1,15 @@
+import dotenv from 'dotenv';
+import express from 'express';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
